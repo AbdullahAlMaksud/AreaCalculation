fa0c0246d9872071b8a24e737c0cabaec33ae13f
AbdullahAlMaksud <maksud.workspace@gmail.com>
2024-02-17 15:39:37 +0600

    New JavaScript Added

--- scripts/script.js
+++ scripts/script.js
@@ -1,77 +1,39 @@
+const btn = document.querySelectorAll('.btn');
+let count = 0;
+for (const allButton of btn) {
+    allButton.addEventListener('click', function (e) {
+        const firstInput = e.target.parentNode.parentNode.children[2].children[0].children[0];
+        const firstInputValue = parseFloat(firstInput.value);
+        const secondInput = e.target.parentNode.parentNode.children[2].children[1].children[0];
+        const secondInputValue = parseFloat(secondInput.value);
+        const puttingArea = e.target.parentNode.parentNode.children[3].children[0].children[0];
+        const parentName = e.target.parentNode.parentNode.children[0].innerText;
+        let area = 0;
 
-function halfAndTwoInputMultiplication(inputField1, inputField2, put, heading) {
-    const baseInput = document.getElementById(inputField1);
-    const baseText = baseInput.value;
-    const base = parseFloat(baseText);
-    // console.log(base)
-
-    const heightInput = document.getElementById(inputField2);
-    const heightText = heightInput.value;
-    const height = parseFloat(heightText);
-    // console.log(height);
-
-    const areaCalculation = 0.5 * base * height;
-    const puttingArea = document.getElementById(put);
-    puttingArea.innerText = areaCalculation;
-
-    var myVariable = 'Hello';
-    var myText = ' World!';
-    var newText = document.createTextNode(myVariable + myText);
+        const audio = new Audio("./images/click.wav");
+        audio.play();
 
 
-    const AreaResult = areaCalculation;
+        if (parentName === 'Triangle' || parentName === 'Rhombus' || parentName === 'Pentagon') {
+            area = 0.5 * firstInputValue * secondInputValue;
+        }
+        else if (parentName === 'Ellipse') {
+            area = (Math.PI * firstInputValue * secondInputValue).toFixed(2);
+        }
+        else {
+            area = firstInputValue * secondInputValue;
+        }
+        const resultContainer = document.getElementById('result-add-here');
+        count = count + 1
+        puttingArea.innerText = area;
 
-    const AreaText = document.getElementById(heading).innerText;
-    const result = document.createElement('p');
-    const centi = document.getElementById('centi').innerText
-    result.innerText = AreaText+"-----"+AreaResult+"-----"+centi;
-    document.getElementById('resut-add-here').appendChild(result);
-}
-
-function twoInputMultiplication(inputField1, inputField2, put, heading) {
-    const baseInput = document.getElementById(inputField1);
-    const baseText = baseInput.value;
-    const base = parseFloat(baseText);
-    console.log(base)
-
-    const heightInput = document.getElementById(inputField2);
-    const heightText = heightInput.value;
-    const height = parseFloat(heightText);
-    console.log(height);
-
-    const areaCalculation = base * height;
-    const puttingArea = document.getElementById(put);
-    puttingArea.innerText = areaCalculation;
-
-    const AreaResult = areaCalculation;
-    const AreaText = document.getElementById(heading).innerText;
-    const result = document.createElement('p');
-    const centi = document.getElementById('centi').innerText;
-    result.innerText = AreaText+"-----"+AreaResult+"-----"+centi;
-    document.getElementById('resut-add-here').appendChild(result);
-
-}
-function piAndTwoInputMultiplication(inputField1, inputField2, put, heading) {
-    const baseInput = document.getElementById(inputField1);
-    const baseText = baseInput.value;
-    const base = parseFloat(baseText);
-    console.log(base)
-
-    const heightInput = document.getElementById(inputField2);
-    const heightText = heightInput.value;
-    const height = parseFloat(heightText);
-    console.log(height);
-
-    const areaCalculation = Math.PI * base * height;
-    const puttingArea = document.getElementById(put);
-    puttingArea.innerText = areaCalculation.toFixed(3);
-
-
-    const AreaResult = areaCalculation;
-    const AreaText = document.getElementById(heading).innerText;
-    const result = document.createElement('li');
-    const centi = document.getElementById('centi').innerText;
-    result.innerText = AreaText+"-----"+AreaResult+"-----"+centi;
-    document.getElementById('resut-add-here').appendChild(result);
-
+        const areaInMeter = area / 10000;
+        const list = document.createElement('div');
+        list.innerHTML = `<div class="flex justify-between items-center text-lg mb-2">
+        <p class = "w-[30%]">${count}. ${parentName}</p>
+        <p class= "w-[40%] text-center"><span id="areaInMeterPlace${count}">${area} c</span>m<sup>2</sup></p>
+        <button onclick="document.getElementById('areaInMeterPlace${count}').innerHTML=${areaInMeter}"  class="bg-blue-600 text-white px-2 py-1 rounded-md w-[30%]">Convert to m<sup>2</sup></button>
+        </div>`
+        resultContainer.appendChild(list);
+    })
 }
