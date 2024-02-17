4206162442ebce3558f7d881eefca6f898e91d64
AbdullahAlMaksud <maksud.workspace@gmail.com>
2024-02-17 15:47:42 +0600

    Favicon Added

--- scripts/script.js
+++ scripts/script.js
@@ -32,7 +32,7 @@ for (const allButton of btn) {
         list.innerHTML = `<div class="flex justify-between items-center text-base lg:text-lg mb-2">
         <p class = "w-[30%]">${count}. ${parentName}</p>
         <p class= "w-[40%] text-center"><span id="areaInMeterPlace${count}">${area} c</span>m<sup>2</sup></p>
-        <button onclick="document.getElementById('areaInMeterPlace${count}').innerHTML=${areaInMeter}"  class="bg-blue-600 text-white px-2 py-1 rounded-md w-[30%]">Convert to m<sup>2</sup></button>
+        <button onclick="document.getElementById('areaInMeterPlace${count}').innerHTML=${areaInMeter}"  class="bg-blue-600 text-white px-2 py-1 rounded-mgd w-[30%]">Convert to m<sup>2</sup></button>
         </div>`
         resultContainer.appendChild(list);
     })
