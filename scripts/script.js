1d88b09b3122ab2f961af3b3579c5b4a92a02d1a
AbdullahAlMaksud <maksud.workspace@gmail.com>
2024-02-17 15:43:27 +0600

    Mobile font size fixed

--- scripts/script.js
+++ scripts/script.js
@@ -29,7 +29,7 @@ for (const allButton of btn) {
 
         const areaInMeter = area / 10000;
         const list = document.createElement('div');
-        list.innerHTML = `<div class="flex justify-between items-center text-lg mb-2">
+        list.innerHTML = `<div class="flex justify-between items-center text-base lg:text-lg mb-2">
         <p class = "w-[30%]">${count}. ${parentName}</p>
         <p class= "w-[40%] text-center"><span id="areaInMeterPlace${count}">${area} c</span>m<sup>2</sup></p>
         <button onclick="document.getElementById('areaInMeterPlace${count}').innerHTML=${areaInMeter}"  class="bg-blue-600 text-white px-2 py-1 rounded-md w-[30%]">Convert to m<sup>2</sup></button>
